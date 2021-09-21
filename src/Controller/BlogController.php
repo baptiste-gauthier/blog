2cf6864927c7e98626bd263398f1aaedeb88ff94
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2021-09-21 17:07:21 +0200

    ajout du dd

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
@@ -13,8 +14,13 @@ class BlogController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repo->findAll(); 
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -26,9 +32,16 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show() {
-        return $this->render('blog/show.html.twig'); 
+    public function show($id) {
+        $repo = $this->getDoctrine()->getRepository(Article::class); 
+
+        $article = $repo->find($id); 
+
+        dd($article); 
+
+        return $this->render('blog/show.html.twig' ,[
+            'article' => $article ]); 
     }
 }
--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -4,13 +4,22 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
 
 class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 0; $i <= 10; $i++) {
+
+            $article = new Article();
+            $article->setTitle("Titre  de l'article n°$i")
+                    ->setContent("<p>Contenu de l'article $i</p>")
+                    ->setImage("http://placehold.it/350x150")
+                    ->setCreatedAt(new \DateTimeImmutable());
+            
+            $manager->persist($article);
+        }
 
         $manager->flush();
     }
