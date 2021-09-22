0df47faa849c7eab971409f720abed952cc9ff3d
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2021-09-22 14:13:38 +0200

    mis en place form
    form ajout article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,16 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
+// use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-
         $articles = $repo->findAll(); 
 
         return $this->render('blog/index.html.twig', [
@@ -30,18 +33,36 @@ class BlogController extends AbstractController
     public function home() {
         return $this->render('blog/home.html.twig') ;
     }
+    
+    /**
+     * @Route("/blog/new" , name="blog_create")
+     */
+    public function create() {
+        
+        $article = new Article() ; 
 
+        $form = $this->createFormBuilder($article)
+                    ->add('title' , TextType::class , [
+                        'attr' => [
+                            'placeholder' => "Titre de l'article"
+                        ]
+                    ])
+                    ->add('content')
+                    ->add('image')
+                    ->getForm(); 
+        return $this->render('blog/create.html.twig' , [
+            'formArticle' => $form->createView()
+        ]) ; 
+    }
+    
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id) {
-        $repo = $this->getDoctrine()->getRepository(Article::class); 
-
-        $article = $repo->find($id); 
-
-        dd($article); 
+    public function show(Article $article) {
+        // dd($article); 
 
         return $this->render('blog/show.html.twig' ,[
             'article' => $article ]); 
     }
+
 }
--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -5,18 +5,21 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use Faker\Factory;
 
 class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 0; $i <= 10; $i++) {
+        $faker = Factory::create('fr_FR');
+
+        for($i = 1; $i <= 10; $i++) {
 
             $article = new Article();
-            $article->setTitle("Titre  de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article $i</p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTimeImmutable());
+            $article->setTitle("$faker->sentence")
+                    ->setContent("<p>$faker->text</p>")
+                    ->setImage($faker->imageUrl(350, 150, 'animals', true))
+                    ->setCreatedAt(new \DateTimeImmutable($faker->date('Y-m-d H:i:s')));
             
             $manager->persist($article);
         }
